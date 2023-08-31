0f82e2820f9a16430d3c104efade05a90a360b69
Owner <Owner@DESKTOP-BMQUE5Q>
2023-08-31 11:31:21 -0600

    remove address property

--- WebApi/Models/Address.cs
+++ WebApi/Models/Address.cs
@@ -14,8 +14,7 @@ namespace WebApi.Models
         public string Address1 { get; set; }
 
         public string Address2 { get; set; }
-        public string Address3 { get; set; }
-
+       
 
     }
 }
